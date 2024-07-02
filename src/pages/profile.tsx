6fed45f2126dbfc58c7078df55bbc4b43570c27a
JJ Consulting <jjconsultingirvine@gmail.com>
2024-07-02 16:56:23 -0700

    Changes

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -10,47 +10,75 @@ const Profile: React.FC<Props> = (props: Props) => {
   const [data, setData] = useState(null as any);
   const navigate = useNavigate();
   const session = useSession().session;
+  console.log(session);
+  const [experiences, setExperiences] = useState([] as any[]);
   useEffect(() => {
-    supabase(session).then(sup=>sup
-      .from("profiles")
-      .select()
-      .eq("Name", params.profile_id)
-      .then((data) => {
-        console.log(data);
-        setData(data.data![0]);
-      }));
+    supabase(session).then((sup) =>
+      sup
+        .from("profiles")
+        .select()
+        .eq("Name", params.profile_id)
+        .then((data) => {
+          console.log(data);
+          setData(data.data![0]);
+        })
+    );
   }, [session]);
-  console.log(data);
+
+  useEffect(() => {
+    supabase(session)
+      .then((sup) =>
+        sup.from("experiences").select()
+      )
+      .then((exps) => setExperiences(exps.data!));
+  },[session]);
+  console.log(experiences);
+  console.log(session?.user.id);
   return (
     <div className="org_page_outer">
-        <div className="org_page">
-      <div className="page_header">
-        <button className="back_button" onClick={() => navigate(-1)}>
-          <svg
-            height="24px"
-            id="Layer_1"
-            version="1.1"
-            viewBox="0 0 512 512"
-            width="24px"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 " />
-          </svg>
-        </button>
-        {data && <div>{data.name}</div>}
-      </div>
-      {data && (
-          <div>
-          <img
-            className="org_pfp"
-            src={
-              data.pfp ||
-              "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
-            }
-          ></img>
-          <div>{data.Name}</div>
-          </div>
-      )}
+      <div className="org_page">
+        <div className="page_header">
+          <button className="back_button" onClick={() => navigate(-1)}>
+            <svg
+              height="24px"
+              id="Layer_1"
+              version="1.1"
+              viewBox="0 0 512 512"
+              width="24px"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <polygon points="352,128.4 319.7,96 160,256 160,256 160,256 319.7,416 352,383.6 224.7,256 " />
+            </svg>
+          </button>
+          {data && <div>{data.Name}</div>}
+        </div>
+        {data && (
+          <>
+            <div className="horizontal">
+              <img
+                className="org_pfp"
+                src={
+                  data.pfp ||
+                  "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
+                }
+              ></img>
+
+              <div className="org_page_short_desc">
+                {data.description || "User on {app name}"}
+              </div>
+
+            </div>
+            <div className="experiences_list">
+                {experiences.map(exp => {
+                    return <div className="experience">
+                        {JSON.stringify(exp)}
+                    </div>;
+                })}
+
+
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
