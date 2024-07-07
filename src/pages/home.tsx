1712bf3a78d84dead640777350b9ca22364f943b
Benjamin Cates <thebenjicat@gmail.com>
2024-07-06 23:57:29 -0700

    Fix home page header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,11 +6,11 @@ import { Organization, User } from "../schema";
 import { SupabaseClient } from "@supabase/supabase-js";
 
 interface Props {
-    supabase: SupabaseClient<any, "public", any>
-    clerk_session: any
-    user: User | null
-    orgs: Organization[]
-    setUser: (usr: User) => void,
+  supabase: SupabaseClient<any, "public", any>;
+  clerk_session: any;
+  user: User | null;
+  orgs: Organization[];
+  setUser: (usr: User) => void;
 }
 
 const Home: React.FC<Props> = (props: Props) => {
@@ -22,13 +22,13 @@ const Home: React.FC<Props> = (props: Props) => {
     } else {
       new_saved.push(name);
     }
-    props.setUser({ ...(props.user), saved: new_saved });
+    props.setUser({ ...props.user, saved: new_saved });
   };
   let saved_list = [] as Organization[];
   let recommended_list = [] as Organization[];
   let unsaved_list = [] as Organization[];
   (props.orgs || []).forEach((org) => {
-    if (props.user && (props.user.saved).includes(org.url_name))
+    if (props.user && props.user.saved.includes(org.url_name))
       saved_list.push(org);
     else if (
       props.user &&
@@ -47,41 +47,43 @@ const Home: React.FC<Props> = (props: Props) => {
   });
 
   return (
-    <div className="page">
+    <div className="outer_page">
       <TopNavBar title="Volunteer App"></TopNavBar>
-      <div className="orgs_list">
-        {saved_list.length != 0 && <h2>Saved</h2>}
+      <div className="page">
         <div className="orgs_list">
-          {saved_list.map((org) => (
-            <OrgListing
-              org={org}
-              saved
-              save_callback={toggle_save}
-              key={org.url_name}
-            ></OrgListing>
-          ))}
-        </div>
-        {recommended_list.length != 0 && <h2>Recommended</h2>}
-        <div className="orgs_list">
-          {recommended_list.map((org) => (
-            <OrgListing
-              org={org}
-              saved={false}
-              save_callback={toggle_save}
-              key={org.url_name}
-            ></OrgListing>
-          ))}
-        </div>
-        {unsaved_list.length != 0 && <h2>All Organizations</h2>}
-        <div className="orgs_list">
-          {unsaved_list.map((org) => (
-            <OrgListing
-              org={org}
-              saved={false}
-              save_callback={toggle_save}
-              key={org.url_name}
-            ></OrgListing>
-          ))}
+          {saved_list.length != 0 && <h2>Saved</h2>}
+          <div className="orgs_list">
+            {saved_list.map((org) => (
+              <OrgListing
+                org={org}
+                saved
+                save_callback={toggle_save}
+                key={org.url_name}
+              ></OrgListing>
+            ))}
+          </div>
+          {recommended_list.length != 0 && <h2>Recommended</h2>}
+          <div className="orgs_list">
+            {recommended_list.map((org) => (
+              <OrgListing
+                org={org}
+                saved={false}
+                save_callback={toggle_save}
+                key={org.url_name}
+              ></OrgListing>
+            ))}
+          </div>
+          {unsaved_list.length != 0 && <h2>All Organizations</h2>}
+          <div className="orgs_list">
+            {unsaved_list.map((org) => (
+              <OrgListing
+                org={org}
+                saved={false}
+                save_callback={toggle_save}
+                key={org.url_name}
+              ></OrgListing>
+            ))}
+          </div>
         </div>
       </div>
     </div>
